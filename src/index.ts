0de793c5cc109e328f5397aa40324ff8e698f35d
Benjamin Kraatz <benjaminkraatz@yahoo.com>
2024-11-19 14:41:20 +0100

    refactor: move types to separate file

--- src/index.ts
+++ src/index.ts
@@ -2,54 +2,16 @@ import { execSync } from "child_process";
 import { existsSync, readFileSync } from "node:fs";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
+import {
+  CommitInfo,
+  Config,
+  Package,
+  VersionChanges,
+  VersionUpdate,
+} from "./types.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-type NonScopeBehavior = "bump" | "ignore";
-
-interface Config {
-  versionedPackages: Package[];
-  /**
-   * Determines the behavior when a commit message does not contain a scope.
-   * It defaults to "bump", which means the commit will be treated as a version bump,
-   * if it contains one of the conventional commit types.
-   *
-   * "ignore" will not treat this commit as a version bump for a package
-   * that is not in the scope.
-   * @default "bump"
-   */
-  nonScopeBehavior?: NonScopeBehavior;
-}
-
-interface Package {
-  name: string;
-  tagPrefix: string;
-  directory: string;
-  dependsOn: string[];
-}
-
-interface VersionChanges {
-  major: boolean;
-  minor: boolean;
-  patch: boolean;
-}
-
-interface VersionUpdate {
-  tagPrefix: string;
-  currentVersion: string;
-  nextVersion: string;
-  hasChanges: boolean;
-  changes: CommitInfo[];
-}
-
-interface CommitInfo {
-  hash: string;
-  message: string;
-  type: string;
-  breaking: boolean;
-  reasons: string[];
-}
 
 // Load configuration
 const CONFIG_PATH = resolve(process.cwd(), "release-config.json");
